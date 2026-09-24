63d6587e7256e22e54ee67a9e7a168b5e24b2cc7
Saba Karaman <sabakaraman@gmail.com>
2026-09-24 16:41:34 -0600

    ModelFlow services with Docker

--- src/main/java/com/saba/modelflow/PredictionController.java
+++ src/main/java/com/saba/modelflow/PredictionController.java
@@ -35,7 +35,7 @@ public class PredictionController {
         HttpClient client = HttpClient.newHttpClient();
 
         HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("http://localhost:8000/predict"))
+            .uri(URI.create("http://modelflow-python-container:8000/predict"))
             .header("Content-Type", "application/json")
             .POST(HttpRequest.BodyPublishers.ofString(
                 "{\"text\":\"" + text + "\"}"
